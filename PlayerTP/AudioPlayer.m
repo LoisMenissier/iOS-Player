1476b238433e18c3ee802fd7c2282856b1a3f538
LoisMenissier <lois@lois.lois>
2015-03-20 17:17:03 +0100

    all

---
+++ PlayerTP/AudioPlayer.m
@@ -0,0 +1,37 @@
+//
+//  AudioPlayer.m
+//  PlayerTP
+//
+//  Created by Loïs Ménissier on 18/03/2015.
+//  Copyright (c) 2015 Loïs Ménissier. All rights reserved.
+//
+
+#import "AudioPlayer.h"
+
+@interface AudioPlayer ()
+
+@end
+
+@implementation AudioPlayer
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ PlayerTP/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  PlayerTP
+//
+//  Created by Loïs Ménissier on 18/03/2015.
+//  Copyright (c) 2015 Loïs Ménissier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
